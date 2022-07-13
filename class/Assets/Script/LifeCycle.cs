441693873a39a3d940388fcc218af2bc39763ed6
cksgh3004 <cksgh3004@naver.com>
2022-07-13 19:50:09 +0900

    Tiger

---
+++ class/Assets/Script/LifeCycle.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeCycle : MonoBehaviour
+{
+
+    int health = 0;
+
+    private void Awake()
+    {
+        Debug.Log("Tiger Awake");
+    }
+
+    void Start()
+    {
+        Debug.Log("Tiger Start");
+    }
+
+    private void OnEnable()
+    {
+        Debug.Log("Tiger OnEnable");
+    }
+
+    private void FixedUpdate()
+    {
+        Debug.Log("Tiger FixedUpdate");
+    }
+
+    private void Update()
+    {
+        health++;
+
+        Debug.Log(health);
+
+        if (health>=100)
+        {
+            health = 0;
+        }
+    }
+
+}
