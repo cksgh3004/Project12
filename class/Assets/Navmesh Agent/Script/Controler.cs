dd9137cfe931bda7181827df3982943ea99328ac
cksgh3004 <cksgh3004@naver.com>
2022-07-20 19:46:54 +0900

    Navmesh Agent

---
+++ class/Assets/Navmesh Agent/Script/Controler.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Controler : MonoBehaviour
+{
+    public float speed = 5.0f;
+    private NavMeshAgent agent;
+    void Start()
+    {
+        agent=GetComponent<NavMeshAgent>();
+
+    }
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray=Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                Move(hit.point);
+            }
+        }
+    }
+    public void Move(Vector3 Position)
+    {
+        agent.speed = speed;
+        agent.SetDestination(Position);
+    }
+}
